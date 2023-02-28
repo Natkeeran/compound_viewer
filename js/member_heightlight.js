561d66045f9f08f0ae42cca360362e043308d567
Natkeeran <natkeeran@gmail.com>
2023-02-28 00:24:50 -0500

    wraping js inside Drupal behavior

--- js/member_heightlight.js
+++ js/member_heightlight.js
@@ -1,15 +1,26 @@
+/*
+This function highlights (provides a box around) the selected compound member item.
 
+It checks to see if compound_member_node_id is in the url, if so
+selects the member block specified in the url
+else, loads the first member block.
+*/
 
+(function($) {
+  Drupal.behaviors.myBehavior = {
+    attach: function (context, settings) {
 
-jQuery(document).ready(function(){
-  var current_url = String(window.location);
+      var current_url = String(window.location);
 
-  if (current_url.indexOf("compound_member_node_id") > 0) {
-    var selected_link = jQuery('#block-views-block-compound-object-members-block-1').find("a[href='" + current_url + "']");
-    var selected_block = selected_link.closest('.views-view-grid__item');
-    selected_block.addClass("compound_member_selected");
-  } else {
-    var selected_block = jQuery('#block-views-block-compound-object-members-block-1').find(".views-view-grid__item").first();
-    selected_block.addClass("compound_member_selected");
-  }
-});
+      if (current_url.indexOf("compound_member_node_id") > 0) {
+        var selected_link = $('#block-views-block-compound-object-members-block-1').find("a[href='" + current_url + "']");
+        var selected_block = selected_link.closest('.views-view-grid__item');
+        selected_block.addClass("compound_member_selected");
+      } else {
+        var selected_block = $('#block-views-block-compound-object-members-block-1').find(".views-view-grid__item").first();
+        selected_block.addClass("compound_member_selected");
+      }
+
+    }
+  };
+  })(jQuery);
